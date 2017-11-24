a7a94f6477915872286f6c3dbad3f658539c638a
afarable-1997@hormail.com <afarable-1997@hormail.com>
2017-11-23 22:10:14 -0500

    any

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,18 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
+    controllerAs: 'vm',
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl',['$scope','$http' ,function($scope, $http) {
+var vm = this;
+vm.productos =[];
+$http.get('http://calidad.dev/api/produtos/producto/index',{})
+.then(function(response){
+  
+  vm.productos = response.data;
+  console.log(vm.productos);
+})
 }]);
